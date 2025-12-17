5c7eab4fea1355cbdc735d199d957b8cd15a49f7
Jos Geerink <Jos@MacBook.local>
2025-12-17 11:52:56 +0100

    Send userstory to server

--- script/script.js
+++ script/script.js
@@ -2,6 +2,10 @@ const DEV_URL = "http://localhost:8080/";
 const LIVE_URL = "http://wiresnchains.com/";
 const LIVE_MODE = false;
 
+
+const DEV_MODE = true;
+const BASE_URL = DEV_MODE ? 'localhost:8080' : 'wiresnchains.com';
+
 function buildQuery(params = {}) {
     const query = new URLSearchParams(params).toString();
     return query ? `?${query}` : "";
@@ -32,9 +36,9 @@ async function request(path, method, data = {}) {
     return payload;
 }
 
-(async () => {
-    console.log(await request("create-game", "POST"));
-})();
+// (async () => {
+//     console.log(await request("create-game", "POST"));
+// })();
 
 function copyBtn() {
     const copyText = document.getElementById("connection-code");
@@ -57,7 +61,7 @@ function copyBtn() {
     }, 1000);
 }
 
-function submitBtn() {
+async function submitBtn() {
     const game = document.getElementById("game");
     const submitText = document.getElementById("submit-text");
     const submitBtn = document.getElementById("submit");
@@ -84,4 +88,54 @@ function submitBtn() {
     submitBtn.style.display = 'none';
     game.style.display = 'none';
     submitText.style.display = 'inline-flex';
+
+
+    console.log(123);
+
+    const div = document.querySelector('div#game');
+    console.log(div);
+    const nodeList = div.querySelectorAll('input');
+    console.log(nodeList);
+
+    
+    const input = [...nodeList].map(input => input.value);
+    console.log(input);
+    const userStory = input.join(' ');
+    console.log(userStory);
+    
+    
+
+
+    console.log('sending request...');
+    const response = await request("create-game", "POST");
+    console.log(response);
+
+
+
+    
+    const gameID = response.connectionCode;
+    const player = 'Jos';
+
+    const url = `ws://${BASE_URL}/game/${gameID}/${player}`;
+    console.log('url: ');
+    console.log(url);
+    
+    const ws = new WebSocket(url);
+    const data = {
+        event: 'send-user-story',
+        data: {
+            message: userStory,
+            // as:
+            // wantTo:
+            // soThat: 
+        },
+    };
+
+    const json = JSON.stringify(data);
+
+    ws.addEventListener('open', (event) => {
+        console.log("OPEN");
+        ws.send(json);
+    })
+    // onopen = (event) => { }
 }
